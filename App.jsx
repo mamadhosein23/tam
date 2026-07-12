4bb84402d26a9511d879ae8f0238dee33da74ea4
Mohamad Hosein Gholami <23mhghm23@gmail.com>
2026-07-12 15:09:21 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 const machines = [
-  { id: 1, title: "تعمیر برد", price: 500000, isAvailable: true },
+  { id: 1, title: "تعمیر برد", price: 5000000, isAvailable: true },
   { id: 2, title: "تعویض پمپ", price: 30000, isAvailable: false },
   { id: 3, title: "جرم‌گیری", price: 150000, isAvailable: true }
 ];
