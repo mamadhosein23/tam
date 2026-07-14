dd2607151b5d2179452880ac622c0357b999dd2d
Mohamad Hosein Gholami <23mhghm23@gmail.com>
2026-07-14 23:03:22 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-// data/machines.js
+// data/machines.j
 export const machines = [
   { id: 1, title: "تعمیر برد", price: 50000, isAvailable: true },
   { id: 2, title: "تعویض پمپ", price: 30000, isAvailable: false },
