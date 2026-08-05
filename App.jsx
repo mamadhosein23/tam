6c18a2a2b41c1b06306638867e19ca1d23674fc2
Mohamad Hosein Gholami <23mhghm23@gmail.com>
2026-08-05 08:57:17 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import { createRoot } from "react-dom/client";
 
 const API_URL = "https://api.tamironlineesfahan.ir/services"; // بک‌اند FastAPI خودت
